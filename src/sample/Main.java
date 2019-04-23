36713b8f52a7e62ac02fa16ea727f626d3cef6e4
Dmitriy Staroverov <star492@mail.ru>
2019-04-23 19:44:48 +0400

    add css

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Java Chat");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.getIcons ().add(new Image ( "file:src/img/hashtag.png" )) ;
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
